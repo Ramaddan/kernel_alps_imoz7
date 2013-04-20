fa6d55283d99634324106c0635d8f80f5e86b51d
pdroid <pdroid@MS-7596-ubuntu.(none)>
2013-04-20 16:40:09 +0200

    fix minor errors

--- kernel/out/.missing-syscalls.d
+++
@@ -1,6 +0,0 @@
--:  include/generated/autoconf.h \
- /home/pdroid/CM9/kernel/wiko/s8073/kernel/arch/arm/include/asm/types.h \
- /home/pdroid/CM9/kernel/wiko/s8073/kernel/include/asm-generic/int-ll64.h \
- /home/pdroid/CM9/kernel/wiko/s8073/kernel/arch/arm/include/asm/bitsperlong.h \
- /home/pdroid/CM9/kernel/wiko/s8073/kernel/include/asm-generic/bitsperlong.h \
- /home/pdroid/CM9/kernel/wiko/s8073/kernel/arch/arm/include/asm/unistd.h
--- kernel/out/usr/.initramfs_data.cpio.d
+++
@@ -1,1 +0,0 @@
-deps_initramfs := /home/pdroid/CM9/kernel/wiko/s8073/kernel/scripts/gen_initramfs_list.sh \
