7790dad2b5931e06783148c61d9f2d602fcebf8d
Ramaddan <ramaddan@gmx.com>
2021-04-14 09:23:00 +0300

    Update dsi_drv.c
    Updated mdelay call for precision

--- kernel/mediatek/platform/mt6577/kernel/drivers/video/dsi_drv.c
+++ kernel/mediatek/platform/mt6577/kernel/drivers/video/dsi_drv.c
@@ -121,7 +121,7 @@ static long int get_current_time_us(void)
 static void lcm_mdelay(UINT32 ms)
 {
     //udelay(1000 * ms);
-    mdelay(1000 * ms);
+    mdelay((1000 * ms) / 1000);
 }
 
 #if ENABLE_DSI_INTERRUPT
